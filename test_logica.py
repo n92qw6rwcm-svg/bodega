1a5bddd057cb16ca4a843a59b62b0c0c6bacdcb2
Jose López <josejlpzp+github@gmail.com>
2026-06-02 04:11:50 -0500

    test: implementar fixtures y refactorizar 9 pruebas unitarias

--- test_logica.py
+++ test_logica.py
@@ -3,39 +3,106 @@ tests
 '''
 
 import pytest
-from logica import agregar_producto
+from logica import agregar_producto, registrar_entrada, registrar_salida
 
 
-def test_agregar_producto_caso_normal():
-    inventario = []
-    agregar_producto(inventario, "arroz", 10, 5)
-    assert len(inventario) == 1
-    assert inventario[0]["nombre"] == "arroz"
+@pytest.fixture(name='inventario_vacio')
+def crear_lista_vacia():
+    ''' . '''
+    return []
 
 
-def test_agregar_producto_nombre_vacio():
-    inventario = []
+@pytest.fixture(name='inventario_test')
+def inventario_con():
+    ''' . '''
+    return [
+        {'nombre': 'pan',
+         'cantidad': 10,
+         'stock_minimo': 5}
+    ]
+
+
+def test_agregar_producto_caso_normal(inventario_vacio):
+    '''
+    test
+    '''
+    agregar_producto(inventario_vacio, "arroz", 10, 5)
+    assert len(inventario_vacio) == 1
+    assert inventario_vacio[0]["nombre"] == "arroz"
+
+
+def test_agregar_producto_nombre_vacio(inventario_vacio):
+    '''
+    test
+    '''
     with pytest.raises(ValueError):
-        agregar_producto(inventario, '', 10, 5)
+        agregar_producto(inventario_vacio, '', 10, 5)
 
 
-def test_agregar_producto_nombre_repetido():
-    inventario = [{'nombre': 'coco', 'cantidad': 4, 'stock_minimo': 2}]
+def test_agregar_producto_nombre_repetido(inventario_test):
+    '''
+    test
+    '''
     with pytest.raises(ValueError):
-        agregar_producto(inventario, 'coco', 3, 1)
-    assert len(inventario) == 1
+        agregar_producto(inventario_test, 'pan', 3, 1)
+    assert len(inventario_test) == 1
 
 
-def test_agregar_producto_cantidad_negativa():
-    inventario = [{'nombre': 'coco', 'cantidad': 4, 'stock_minimo': 2}]
+def test_agregar_producto_cantidad_negativa(inventario_test):
+    '''
+    test
+    '''
     with pytest.raises(ValueError):
-        agregar_producto(inventario, 'mango', -1, 1)
+        agregar_producto(inventario_test, 'mango', -1, 1)
 
 
-def test_agregar_producto_stock_minimo_negativo():
-    inventario = [{'nombre': 'coco', 'cantidad': 4, 'stock_minimo': 2}]
+def test_agregar_producto_stock_minimo_negativo(inventario_test):
+    '''
+    test
+    '''
     with pytest.raises(ValueError):
-        agregar_producto(inventario, 'mango', 3, -1)
+        agregar_producto(inventario_test, 'mango', 3, -1)
 
 
-# consultar si hacer los test debe agregarse en un commit. De hecho está sesión se trato de eso. Lo único que se agrego fue este archivo test_logica.py y se aqgregaron 5 test y en lógica, se agregó una condición en la función de agregar. Dicha función surge de un error que se encontró al diseñar un test. La documentación sugería una validación, pero la implementación no la reflejaba.
+def test_registrar_entrada_caso_normal(inventario_test):
+    '''
+    Verificar que cuando el producto extiste y las unidades son válidas,
+    la cantidad se actualiza correctamente
+    Recibe un inventario con al menos un producto, 
+    un nombre que existe en él y un número positivo de unidades a agregar o cero
+    Verifica que la cnatidad del producto en el inventario aumente exactamente 
+    en el valor de unidades a agregar
+    '''
+
+    registrar_entrada(inventario_test, 'pan', 5)
+    assert inventario_test[0]['cantidad'] == 15
+
+
+def test_resgistar_entrada_producto_no_existe(inventario_test):
+    '''
+    test
+    '''
+    with pytest.raises(ValueError):
+        registrar_entrada(inventario_test, 'arroz', 5)
+
+
+def test_registar_entrada_unidades_negativas(inventario_vacio):
+    '''
+    Qué hace -> verificar si registrar entrada lanza ValueError si las unidades a agregar son menores a cero
+    Qué recibe -> recibe una lista vacía, nombre y unidades a agregar negativas
+    Qué verifica -> Que se lance un ValueError si las unidades a agregar son menores a cero
+    '''
+
+    with pytest.raises(ValueError):
+        registrar_entrada(inventario_vacio, 'harina', -1)
+
+
+def test_registar_entrada_unidades_cero(inventario_test):
+    '''
+    test
+    '''
+    registrar_entrada(inventario_test, 'pan', 0)
+    assert inventario_test[0]['cantidad'] == 10
+
+
+# def test_registrar_salida_caso_normal():
